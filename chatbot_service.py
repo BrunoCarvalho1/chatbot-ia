71d4dffcea2ae2cdc0addb4222cfa73658daf827
Igor Tauhan <igortauhan.ds31@gmail.com>
2023-10-29 15:06:48 -0300

    Import data from dataset

--- chatbot_service.py
+++ chatbot_service.py
@@ -1,55 +1,16 @@
-import requests
-import utils
-import urllib.parse
-import utils
+import ast
+import csv
 
+def import_data_from_dataset():
+    with open('movies_metadata.csv') as csvfile:
+        for row in csv.DictReader(csvfile):
+            for genre in ast.literal_eval(row['genres']):
+                question = "Me recomende um filme de "
+                response = "Claro! Aqui está um filme de "
 
-def get_movies_or_tv_show_by_genre(genre, endpoint):
-    movies_or_tv_show_array = []
-    pagination_temp = 1
-    pagination_control = True
+                question += f"{genre['name']}\n"
+                response += f"{genre['name']}. Nome do filme: {row['title']}. Descrição do filme: {row['overview']}. Data de lançamento: {row['release_date']}"
 
-    while pagination_control:
-        response = requests.get(utils.api_url + f"/search{endpoint}?query={urllib.parse.quote(genre)}&include_adult=true&language={utils.language}&page={pagination_temp}", headers=utils.get_request_headers()).json()
-        max_pages = response['total_pages']
-
-        if max_pages == pagination_temp:
-            pagination_control = False
-
-        # Controle da paginação
-        if response['total_pages'] != 1:
-            pagination_temp += 1
-
-        for movies_or_tv_show in response['results']:
-            print(movies_or_tv_show)
-            temp = {}
-
-            if endpoint == "/movie":
-                temp['title'] = movies_or_tv_show['title']
-                temp['release_date'] = movies_or_tv_show['release_date']
-
-            if endpoint == "/tv":
-                temp['title'] = movies_or_tv_show['name']
-                temp['release_date'] = movies_or_tv_show['first_air_date']
-
-            temp['overview'] = movies_or_tv_show['overview']
-
-            movies_or_tv_show_array.append(temp)
-
-    return movies_or_tv_show_array
-
-
-def get_genres_array():
-    genres = []
-
-    genres_tmdb_response = get_genres(utils.movie_url)
-
-    for genre in genres_tmdb_response['genres']:
-        genres.append(genre['name'])
-
-    return genres
-
-
-def get_genres(endpoint):
-    genres = requests.get(utils.api_url + f"/genre{endpoint}/list?language={utils.language}", headers=utils.get_request_headers()).json()
-    return genres
+                with open('training_file.txt', 'a') as training_file:
+                    training_file.write(question)
+                    training_file.write(response)
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import chatbot_service
-import utils
-
 
 def main():
-    movies_or_tv_show = chatbot_service.get_movies_or_tv_show_by_genre('Ação', utils.tv_show_url)
-    for movie_or_tv_show in movies_or_tv_show:
-        print(movie_or_tv_show)
+    chatbot_service.import_data_from_dataset()
 
 
 if __name__ == "__main__":
