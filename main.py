5863f0a45af5045159643ccc86d41cbd1d76456e
Igor Tauhan <igortauhan.ds31@gmail.com>
2023-10-30 18:07:19 -0300

    Training file translated

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import translator
 
 def main():
     # Remover os comentários para extrair os dados do csv e formar no txt
-    # chatbot_service.import_data_from_dataset()
+    chatbot_service.import_data_from_dataset()
 
     # Remover o comentário para treinar o bot
     # my_chatbot.start_training()
-    pass
 
 
 if __name__ == "__main__":
