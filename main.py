476a283a49c67f22d04c675f374b74aa4472aca2
Igor Tauhan <igortauhan.ds31@gmail.com>
2023-10-31 08:31:03 -0300

    Setup telegram bot

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import my_chatbot
 import chatbot_service
-import translator
+import my_telegram_bot
 
 
 def main():
     # Remover os comentários para extrair os dados do csv e formar no txt
-    chatbot_service.import_data_from_dataset()
+    # chatbot_service.import_data_from_dataset()
 
     # Remover o comentário para treinar o bot
     # my_chatbot.start_training()
+
+    # Instância do bot do telegram
+    my_telegram_bot.my_bot_instance()
 
 
 if __name__ == "__main__":
---
+++ my_telegram_bot.py
@@ -0,0 +1,27 @@
+import os
+from telegram import Update
+from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+import my_chatbot
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    movie = my_chatbot.conversation(update.message.text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=str(movie))
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, world!")
+
+
+def my_bot_instance():
+    # Instância do bot do telegram
+    bot = ApplicationBuilder().token(os.getenv('bot_token')).build()
+
+    start_handler = CommandHandler('start', start)
+    message_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    bot.add_handler(start_handler)
+    bot.add_handler(message_handler)
+
+    bot.run_polling()
+
