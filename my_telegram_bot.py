87656964d6368b1d2a436d3a109ec0131a0d6390
Igor Tauhan <igortauhan.ds31@gmail.com>
2023-11-05 10:15:45 -0300

    Adjust response

--- my_telegram_bot.py
+++ my_telegram_bot.py
@@ -1,13 +1,32 @@
 import os
 from telegram import Update
 from telegram.ext import filters, ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+import re
+from datetime import datetime
 import my_chatbot
 import translator
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    response = my_chatbot.conversation(update.message.text)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=str(response))
+    response = str(my_chatbot.conversation(update.message.text))
+
+    # Formatando o nome
+    response = re.sub(r'(?=Nome do filme: )', "\n\n", response)
+
+    # Formatando a descrição
+    response = re.sub(r'(?=Descrição: )', "\n\n", response)
+
+    # Formatando a data
+    response = re.sub(r'(?=Data de lançamento: )', "\n\n", response)
+    try:
+        old_date = response.split('Data de lançamento: ')[1]
+        old_date = datetime.strptime(old_date, "%Y-%m-%d")
+        response = response[:-10]
+        response += old_date.strftime('%d/%m/%Y')
+    except:
+        pass
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
